4fcc90fe5cd987d7fffaa5e843a18a94d78e1a01
Dez <130031332+E-r-r-o-rr@users.noreply.github.com>
2025-11-03 20:49:46 +1100

    Add additional Espresso coverage for dashboard and library

--- app/src/androidTest/java/com/example/automation/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/automation/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.automation
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.action.ViewActions.click
@@ -8,11 +9,13 @@ import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -75,5 +78,55 @@ class MainActivityTest {
 
         onView(withText(R.string.create_title))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun dashboardEmptySectionsAreVisibleWhenNoData() {
+        onView(withId(R.id.dashboardScroll)).perform(swipeUp(), swipeUp())
+
+        onView(withId(R.id.emptyCurrentTask))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.emptyQueued))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.emptyCompleted))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun dashboardCreateButtonOpensCreateScreen() {
+        onView(withId(R.id.dashboardScroll)).perform(swipeUp())
+        onView(allOf(withId(R.id.createItemButton), isDisplayed())).perform(click())
+
+        onView(withText(R.string.create_title))
+            .check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(R.id.subtitle))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun libraryEmptyStateCreateShortcutOpensCreateScreen() {
+        onView(allOf(withId(R.id.learningListFragment), isDisplayed())).perform(click())
+
+        onView(withId(R.id.libraryScroll)).perform(swipeUp())
+        onView(allOf(withId(R.id.emptyCreateButton), isDisplayed())).perform(click())
+
+        onView(withText(R.string.create_title))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun libraryFilterDefaultsToAllStatus() {
+        onView(allOf(withId(R.id.learningListFragment), isDisplayed())).perform(click())
+
+        onView(withId(R.id.chipAll))
+            .check(matches(isChecked()))
+        onView(withId(R.id.chipTodo))
+            .check(matches(not(isChecked())))
+        onView(withId(R.id.chipDoing))
+            .check(matches(not(isChecked())))
+        onView(withId(R.id.chipDone))
+            .check(matches(not(isChecked())))
     }
 }
